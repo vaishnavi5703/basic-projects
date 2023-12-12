69e1b04787d638dddb873d2b4f5e879cb8c6fee1
vaishnavi5703 <122341054+vaishnavi5703@users.noreply.github.com>
2023-12-12 09:12:50 +0530

    Add files via upload

---
+++ water Drop Clock/index.js
@@ -0,0 +1,24 @@
+let hr = document.getElementById('h');
+let mn = document.getElementById('m');
+let sc = document.getElementById('s');
+
+function displayTime() {
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    
+    let hrotation = 30 * hh + mm / 2;
+    let mrotation = 6 * mm;
+    let srotation = 6 * ss;
+    
+    h.style.transform = `rotate(${hrotation}deg)`;
+    m.style.transform = `rotate(${mrotation}deg)`;
+    s.style.transform = `rotate(${srotation}deg)`;
+
+
+}
+setInterval(displayTime, 1000);
+displayTime();
+
+
